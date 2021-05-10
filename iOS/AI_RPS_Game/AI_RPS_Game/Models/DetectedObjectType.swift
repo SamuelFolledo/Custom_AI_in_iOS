b224eea307118e1bf9abf32213bd8c6a82952577
Samuel Folledo <samuelfolledo@gmail.com>
2021-05-10 03:32:24 -0400

    [add] a way to initializer object type with string instead of rawValue [add] support for keyboard, macbook, spray bottle, hand sanitizer

--- iOS/AI_RPS_Game/AI_RPS_Game/Models/DetectedObjectType.swift
+++ iOS/AI_RPS_Game/AI_RPS_Game/Models/DetectedObjectType.swift
@@ -8,8 +8,8 @@
 import UIKit.UIColor
 import Vision.VNObservation
 
-enum DetectedObjectType: String {
-    case macMini, clock, pinkStickyNote, yellowStickyNote, scissor, rock, paper, pen, remote, applePencil, unknown
+enum DetectedObjectType {
+    case macMini, clock, pinkStickyNote, yellowStickyNote, scissor, rock, paper, pen, remote, applePencil, keyboard, macbook, handSanitizer, sprayBottle, unknown
     var color: UIColor {
         switch self {
         case .rock:
@@ -42,38 +42,81 @@ enum DetectedObjectType: String {
     
     init(trackedObject: VNRecognizedObjectObservation) {
         guard let label = trackedObject.labels.first?.identifier,
-              let objectType = DetectedObjectType(rawValue: label) else {
+              let objectType = DetectedObjectType(name: label) else {
             assertionFailure("Unable to retrieve identifier for result: \(trackedObject)")
             self = .unknown
             return
         }
         self = objectType
     }
+    
+    init?(name: String) {
+        switch name {
+        case "scissor", "scissors":
+            self = .scissor
+        case "rock":
+            self = .rock
+        case "paper":
+            self = .paper
+        case "mac  mini":
+            self = .macMini
+        case "apple  pencil", "applePencil":
+            self = .applePencil
+        case "pinkStickyNote", "pink  sticky  note":
+            self = .pinkStickyNote
+        case "yellowStickyNote", "yellow  sticky  note":
+            self = .yellowStickyNote
+        case "pen":
+            self = .pen
+        case "remote":
+            self = .remote
+        case "clock":
+            self = .clock
+        case "macbook":
+            self = .macbook
+        case "keyboard":
+            self = .keyboard
+        case "sprayBottle", "spray  bottle":
+            self = .sprayBottle
+        case "handSantizer", "hand  sanitizer":
+            self = .handSanitizer
+        default:
+            return nil
+        }
+    }
 }
 
 extension DetectedObjectType {
     var supportedValues: [String] {
         switch self {
-        case .macMini:
-            return ["macMini"]
-        case .clock:
-            return ["clock"]
-        case .pinkStickyNote:
-            return ["pinkStickyNote"]
-        case .yellowStickyNote:
-            return ["yellowStickyNote"]
         case .scissor:
-            return ["scissor"]
+            return ["scissor", "scissors"]
         case .rock:
             return ["rock"]
         case .paper:
             return ["paper"]
+        case .macMini:
+            return ["macMini", "mac  mini"]
+        case .clock:
+            return ["clock"]
+        case .pinkStickyNote:
+            return ["pinkStickyNote", "pink  sticky  note"]
+        case .yellowStickyNote:
+            return ["yellowStickyNote", "yellow sticky  note"]
         case .pen:
             return ["pen"]
         case .remote:
             return ["remote"]
         case .applePencil:
-            return ["applePencil"]
+            return ["applePencil", "apple  pencil"]
+        case .macbook:
+            return ["macbook"]
+        case .keyboard:
+            return ["keyboard"]
+        case .sprayBottle:
+            return ["sprayBottle", "spray  bottle"]
+        case .handSanitizer:
+            return ["handSantizer", "hand  sanitizer"]
         case .unknown:
             return ["unknown"]
         }
