526d7b1ebad921635c66aa3285a043f4a8727227
SamuelFolledo <samuelfolledo@gmail.com>
2022-08-27 16:57:34 -0700

    Implement the object detection
    [add] models from UIKit
    [add] put a layer to show detect object location
    [wip] hook up and create move from detected object

--- iOS Apps/SwiftUI version/RPS Scorer/Shared/Views/CameraCapturePreviewView.swift
+++ iOS Apps/SwiftUI version/RPS Scorer/Shared/Views/CameraCapturePreviewView.swift
@@ -21,21 +21,25 @@ struct CameraCapturePreviewView: UIViewRepresentable {
     }
 }
 
-class UICameraCapturePreviewView: UIView, AVCaptureVideoDataOutputSampleBufferDelegate {
+class UICameraCapturePreviewView: UIView {
     // AVCaptureVideoDataOutputSampleBufferDelegate
     // when you want to handle the video input every second, you will add the delegate
     var recognitionInterval = 0 //Interval for object recognition
+    var captureSession: AVCaptureSession!
+    ///the layers that shows the detected object's location
+    private var overlayLayers = [CALayer]()
+    ///minimum confidence threshold for detected objects
+    private let minConfidence: Float = 0.2
+    
+    var resultLabel: UILabel!
     
     var mlModel: VNCoreMLModel {
         get {
-            guard let model = try? VNCoreMLModel(for: my_custom_model(configuration: MLModelConfiguration()).model) else {
-                fatalError("Could not load ml model")
-            }
+            guard let model = try? VNCoreMLModel(for: my_custom_model(configuration: MLModelConfiguration()).model)
+            else { fatalError("Could not load ml model") }
             return model
         }
     }
-    var captureSession: AVCaptureSession!
-    var resultLabel: UILabel!
     
     func setupSession() {
         captureSession = AVCaptureSession()
@@ -45,41 +49,84 @@ class UICameraCapturePreviewView: UIView, AVCaptureVideoDataOutputSampleBufferDe
         guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else { return }
         guard captureSession.canAddInput(videoInput) else { return }
         captureSession.addInput(videoInput)
-        
         // Output settings
         let output = AVCaptureVideoDataOutput()
         output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoQueue")) // set delegate to receive the data every frame
         if captureSession.canAddOutput(output) {
             captureSession.addOutput(output)
         }
-        
         captureSession.commitConfiguration()
-        
     }
     
     func setupPreview() {
-        self.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        
+        frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = frame
         layer.addSublayer(previewLayer)
-        
-        resultLabel = UILabel()
-        resultLabel.text = ""
-        resultLabel.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 200, width: UIScreen.main.bounds.width, height: 80)
-        resultLabel.textColor = UIColor.black
-        resultLabel.textAlignment = NSTextAlignment.center
-        resultLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        resultLabel.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.7)
-        addSubview(resultLabel)
-        
         captureSession.startRunning()
     }
+}
+
+private extension UICameraCapturePreviewView {
+    func handleObservations(results: [VNRecognizedObjectObservation]) {
+        // remove all previously added masks
+        removeMasks()
+        //filter (unwanted duplicates, low confidence) objects
+        print("Detected \(results.count) objects")
+        let detectedObjects: [DetectedObject] = getDetectedObjects(from: results)
+        //draw the bounding boxes
+        print("Cleaned and left with \(detectedObjects.count)")
+        print("")
+        for object in detectedObjects {
+            drawLayer(in: object.location, color: object.type.color, with: object.confidenceText)
+        }
+//        delegate?.updateDetectedObjects(newDetectedObjects: detectedObjects)
+    }
     
+    ///convert recognizedObject's results to DetectedObject and filter (unwanted duplicates, low confidence) objects
+    func getDetectedObjects(from recognizedObjects: [VNRecognizedObjectObservation]) -> [DetectedObject] {
+        //filter out objects detected with low confidence
+        let highConfidenceObjects = recognizedObjects.filter({ $0.confidence >= minConfidence })
+        // CoreGraphics => transforming origin from top left corner to bottom left corner
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
+        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
+        var detectedObjects: [DetectedObject] = []
+        for trackedObject in highConfidenceObjects { //loop through each detected objects
+            //get trackedObject's location
+            let rectangularLocation = trackedObject.boundingBox.applying(translate).applying(transform)
+            //create a detectedObject model from the trackedObject
+            guard let newDetectedObject = DetectedObject(trackedObject: trackedObject, location: rectangularLocation) else { continue }
+            //loop through each detectedObjects appended already and make sure to remove detectedObjects that has duplicates and lower confidence
+            var shouldAddObject = true
+            for (index, object) in detectedObjects.enumerated() { //loop through each detected objects and make sure there are no duplicates
+                if object.type != newDetectedObject.type {
+                    print("Skipping because \(object.type.rawValue) != \(newDetectedObject.type.rawValue)")
+                    continue
+                }
+                if object.location.isMidXClose(to: newDetectedObject.location) { //if trackedObject intersects with the newDetectedObject in array
+                    if object.confidence < newDetectedObject.confidence { //if newDetectedObject has lower confidence... remove detectedObject
+                        detectedObjects.remove(at: index)
+                    } else {
+                        shouldAddObject = false
+                    }
+                    break
+                } else {
+                    print("Not midx close to for \(object.type.rawValue) and \(newDetectedObject.type.rawValue)")
+                }
+            }
+            if shouldAddObject {
+                detectedObjects.append(newDetectedObject)
+            }
+        }
+        return detectedObjects
+    }
+}
+
+extension UICameraCapturePreviewView: AVCaptureVideoDataOutputSampleBufferDelegate {
     // captureOutput will be called for each frame written
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Recognise the object every 10 frames
-        if recognitionInterval < 10 {
+        // Recognise the object every 5 frames
+        if recognitionInterval < 5 {
             recognitionInterval += 1
             return
         }
@@ -97,18 +144,43 @@ class UICameraCapturePreviewView: UIView, AVCaptureVideoDataOutputSampleBufferDe
             guard let results = request.results as? [VNRecognizedObjectObservation],
                   !results.isEmpty
             else { return }
-            var displayText = ""
-            for result in results.prefix(5) {
-                displayText += "\(result.formattedConfidenceLabel)\n"
-            }
-            print(displayText)
             // Execute it in the main thread
             DispatchQueue.main.async {
-                self.resultLabel.text = displayText
+                self.handleObservations(results: results)
             }
         }
+        request.imageCropAndScaleOption = .scaleFill
         // Execute the request
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
+
+extension UICameraCapturePreviewView {
+    func drawLayer(in rect: CGRect, color: UIColor = .yellow, with label: String) {
+        let mask = CAShapeLayer()
+        let textLayer = CATextLayer()
+        mask.frame = rect
+        textLayer.frame = rect
+        mask.backgroundColor = color.cgColor
+        mask.opacity = 0.2
+        mask.borderColor = UIColor.white.cgColor
+        mask.borderWidth = 2
+        mask.cornerRadius = 12
+        textLayer.string = " "+label
+        textLayer.foregroundColor = color.cgColor
+        textLayer.contentsScale = UIScreen.main.scale
+        textLayer.fontSize = 12
+        overlayLayers.append(mask)
+        overlayLayers.append(textLayer)
+        layer.insertSublayer(mask, at: 1)
+        layer.addSublayer(textLayer)
+    }
+
+    func removeMasks() {
+        for layer in overlayLayers {
+            layer.removeFromSuperlayer()
+        }
+        overlayLayers.removeAll()
     }
 }
 
